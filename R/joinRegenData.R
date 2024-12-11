c82e7f5b62639a5a730bddc59d0ef623c289f986
Stephanie Perles <stephanie_perles@nps.gov>
2024-12-11 13:45:04 -0500

    Changing park argument to work?

--- R/joinRegenData.R
+++ R/joinRegenData.R
@@ -77,7 +77,8 @@
 # Joins microplot tables and filters by park, year, and plot/visit type
 #------------------------
 joinRegenData<-function(speciesType=c('all', 'native','exotic','invasive'), canopyForm=c('canopy','all'),
-  units=c('micro','ha','acres'), park='all',years=2007:2024, QAQC=FALSE, retired=TRUE, anrevisit=FALSE, output, ...){
+  units=c('micro','ha','acres'), park=c('all', 'NERI', 'GARI','BLUE','WV','ALPO','FONE','FRHI','FONE','FLNI','JOFL','WEPA','DEWA'),
+  years=2007:2024, QAQC=FALSE, retired=TRUE, anrevisit=FALSE, output, ...){
 
   park <- match.arg(park)
   status <- match.arg(status)
